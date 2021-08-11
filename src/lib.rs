fb729e49b8c2592927bfc65604976c97adcd6969
ellis <ellis@rwest.io>
2021-08-10 20:24:08 -0500

    description update

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+/// Tenex binary toolbox
+///
+/// Tenex is a collection of General-purpose CLI programs similar in
+/// spirit to busybox. Programs can be bundled into a unified binary,
+/// or executed individually.
+///
+/// * Programs
+///   - weather
+///   - getip
+///   - pack
+///   - dietdb
+///   - serve
+
 #[cfg(test)]
 mod tests {
     #[test]
