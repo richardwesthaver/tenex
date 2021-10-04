40999f935ca3f2c71868a14334ff5bb2167ac234
ellis <ellis@rwest.io>
2021-10-04 04:10:20 -0500

    fmt + discord

--- src/client/discord.rs
+++ src/client/discord.rs
@@ -1,1 +1,12 @@
-
+//! Discord API client
+use rlib::net::http::oauth::client;
+use serde::{Deserialize, Serialize};
+/// The user data we'll get back from Discord.
+/// https://discord.com/developers/docs/resources/user#user-object-user-structure
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+  pub id: String,
+  pub avatar: Option<String>,
+  pub username: String,
+  pub discriminator: String,
+}
