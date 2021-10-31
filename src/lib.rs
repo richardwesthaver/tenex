e36f4c7bd7fade29a78163a0309df4a03f892c85
ellis <ellis@rwest.io>
2021-10-31 05:13:07 -0400

    readme.org update

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,16 @@
-//! Tenex
-//!
-//! A high-level library that provides modules for writing
-//! API clients very quickly.
-//!
-//! This library depends on 'rlib' modules as well as third-party
-//! libraries for specific APIs behind feature flags.
-//!
-//! To use this library, add one or more of the features available:
-//! ```tenex = { version = "0.1.0", features = ["openai", "email", "aws"] }```
+//! lib.rs --- Tenex network clients
+/*!
+A high-level library that provides modules for writing
+API clients very quickly.
+
+This library depends on 'rlib' modules as well as third-party
+libraries for specific APIs behind feature flags.
+
+To use this library, add one or more of the features available:
+
+```tenex = { version = "0.1.0", features = ["openai", "email", "aws"] }```
+*/
+
 pub use rlib::net::Error;
 pub use rlib::util::Result;
 
